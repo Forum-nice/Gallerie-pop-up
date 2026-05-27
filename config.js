72222c6d6a8bdadfe9786ff6a34e19c298c3b631
Forum-nice <lucie.moneyron@ville-nice.fr>
2026-05-27 16:15:19 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ const SUPABASE_URL = 'https://hnnzkazgztyhamsfvoyf.supabase.co'
 const SUPABASE_KEY = 'sb_publishable_PGM2q8iNOqLtVU6ksH53IQ_50wY_BOs'
 const BUCKET_NAME  = 'photos'
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
