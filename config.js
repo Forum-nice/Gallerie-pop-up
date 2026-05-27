10c30978542054556020c0980dbdfdeb01a79f71
Forum-nice <lucie.moneyron@ville-nice.fr>
2026-05-27 16:08:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-// ── À remplir avec tes clés Supabase (étape 2) ──
-const SUPABASE_URL = 'REMPLACE_PAR_TON_URL'
-const SUPABASE_KEY = 'REMPLACE_PAR_TA_CLE_ANON'
+const SUPABASE_URL = 'https://hnnzkazgztyhamsfvoyf.supabase.co'
+const SUPABASE_KEY = 'sb_publishable_PGM2q8iNOqLtVU6ksH53IQ_50wY_BOs'
 const BUCKET_NAME  = 'photos'
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
